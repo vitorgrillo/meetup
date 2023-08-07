71b545611bdcfa2939accd2f74e1cc947af2a802
Vitor Grillo <vgrillo@its.jnj.com>
2023-08-07 16:29:03 -0300

    Creating Meetup application in React.

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+export default function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-faeb9-default-rtdb.firebaseio.com/meetups.json"
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        };
+
+        meetups.push(meetup);
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups);
+    });
+  }, []);
+
+
+
+  if (isLoading) {
+    return (<section><p>Loading...</p></section>);
+  }
+
+  return (
+    <section>
+      <h1>All Mettups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  )
+}
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-faeb9-default-rtdb.firebaseio.com/meetups.json",
+      { 
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
